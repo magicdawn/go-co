399a5d9f1c60553ee968e378b099749cbd2cbe30
magicdawn <magicdawn@qq.com>
2015-05-09 23:08:30 +0800

    add task

---
+++ task/task.go
@@ -0,0 +1,47 @@
+package task
+
+import . "github.com/magicdawn/go-co"
+
+/*
+
+parallel task
+
+*/
+func Parallel(tasks []Task, concurrency int32) (taskRet Task) {
+	taskRet.Channel = make(chan []interface{})
+	taskRet.Result = make([]interface{})
+
+	total := len(tasks)
+	result := make([]interface{}, total)
+
+	running := 0
+	started := 0
+	completed := 0
+
+	if concurrency < 1 {
+		concurrency = 1
+	}
+
+	oncomplete := func() {
+		if completed >= total {
+			t.Channel <- t.Result
+		}
+
+		for started < total && running < concurrency {
+			t := tasks[started]
+			started++
+			running++
+
+			go func(index int, t Task) {
+				taskRet.Result[index] = Await(t)
+
+				running--
+				completed++
+
+				oncomplete()
+			}(running, t)
+		}
+	}
+
+	go oncomplete()
+}
